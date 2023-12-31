ab87a25006ba1956b7d31d9d170387e862918fcb
Shi Hao Ng <shng2025@gmail.com>
2024-01-01 03:15:34 +0800

    Modified .gitignore to exclude executables. And also changed structure of note taking.

--- OpenGL-Series/src/Application.cpp
+++ OpenGL-Series/src/Application.cpp
@@ -1,47 +1,62 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
 
-        glBegin(GL_TRIANGLES);
+	if (glewInit() != GLEW_OK)
+		std::cout << "GLEW Error!" << std::endl;
 
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
+	// Prints in console showcasing OpenGL version, 4.6.0 in this case for my ROG G16
+	std::cout << (const char*)glGetString(GL_VERSION) << std::endl;
 
-        glEnd();
+	
+	float positions[3 * 2] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+	
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * (3 * 2), positions, GL_STATIC_DRAW);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glfwTerminate();
-    return 0;
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
